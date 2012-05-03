002e47907ea42bed6f0b271daadbab5893b96f97
Francesco Capozzo <ximarx@gmail.com>
2012-05-03 10:55:45 +0200

    Introdotta distanza euclidea come peso dei passi per differenziare le due risoluzioni con fattori euristici differenti

--- games/path-finder/Heuristic.py
+++ games/path-finder/Heuristic.py
@@ -15,3 +15,23 @@ def ManhattanDistance(wmemory):
     d = abs( pedone['x'] - goal['x']) + abs( pedone['y'] - goal['y'] )
         
     return d
+
+def EuclideaDistance(oldmemory, newmemory, rule):
+    '''
+    Calcola la distanza percorsa dal pedone
+    durante il passo come distanza euclidea
+    '''
+    
+    from icse.ps.wm.WorkingMemory import WorkingMemory
+    from icse.ps.rules.Regola import Regola
+    
+    assert isinstance(oldmemory, WorkingMemory)
+    assert isinstance(newmemory, WorkingMemory)
+    assert isinstance(rule, Regola)
+    
+    pedone_old = oldmemory.get_facts("Pedone")["Pedone"]
+    pedone_new = newmemory.get_facts("Pedone")["Pedone"]
+    
+    d = pow( pow( pedone_old['x'] - pedone_new['x'], 2) + pow( pedone_old['y'] - pedone_new['y'], 2) , (1./2))
+    
+    return d
